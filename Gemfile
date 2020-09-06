c22783216216bd6408576c3cf45406a4ac5c1e3e
sbatsch <sbatsch2@gmail.com>
2020-09-06 13:09:45 -0500

    jwt and sessions controller added

--- Gemfile
+++ Gemfile
@@ -18,6 +18,9 @@ gem 'jbuilder', '~> 2.7'
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
 gem 'bcrypt', '~> 3.1.7'
+
+gem 'jwt'
+
 gem 'jb'
 gem 'seed_dump'
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     post '/users' => "users#create"
 
+    post "/sessions" => "sessions#create"
 
     get "/prompts" => "prompts#index"
     post "/prompts" => "prompts#create"
